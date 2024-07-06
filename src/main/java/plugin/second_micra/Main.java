5d5e8bf28cd134d17a754714dfe94e4d65c74eec
gobird <monaka@example.com>
2024-07-06 17:13:56 +0900

    first-commit

---
+++ src/main/java/plugin/second_micra/Main.java
@@ -0,0 +1,66 @@
+package plugin.second_micra;
+
+import java.math.BigInteger;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.World;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+  //    public →どこでもアクセスできる  private　→クラスファイルだけアクセスできる
+//       private int count;　→値が0という意味
+  private int count;
+
+  @Override
+  public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(this, this);
+  }
+
+  /**
+   * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
+   *
+   * @param e イベント
+   */
+  @EventHandler
+  public void onPlayerToggleSneak(PlayerToggleSneakEvent e) {
+    // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
+    Player player = e.getPlayer();
+    World world = player.getWorld();
+
+//    素数を定義する
+    BigInteger val = new BigInteger(Integer.toString(count));
+//  valが素数であるかの判定
+    if (val.isProbablePrime(1)) {
+      // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
+      Firework firework = world.spawn(player.getLocation(), Firework.class);
+
+      // 花火オブジェクトが持つメタ情報を取得。
+      FireworkMeta fireworkMeta = firework.getFireworkMeta();
+
+      // メタ情報に対して設定を追加したり、値の上書きを行う。
+      // 今回は青色で星型の花火を打ち上げる。
+      fireworkMeta.addEffect(
+          FireworkEffect.builder()
+              .withColor(Color.BLUE)
+              .with(Type.STAR)
+              .withFlicker()
+              .build());
+      fireworkMeta.setPower(0);
+
+      // 追加した情報で再設定する。
+      firework.setFireworkMeta(fireworkMeta);
+//      素数の花火メッセージ
+      player.sendMessage(val + "は素数です。");
+    }
+    count++;
+  }
+}
